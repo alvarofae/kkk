4b02f5e0b214a673b65b9d2fa5ee73d67dedfce9
alvarofae <105504676+alvarofae@users.noreply.github.com>
2023-11-09 08:48:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ function tocasom(idElementoAudio){
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
-while(contador<listaDeTeclas.lenght){
-    const efeito = listaDeTeclas(contador).classlist[1];
+while(contador < listaDeTeclas.lenght){
+    const efeito = listaDeTeclas[contador].classList[1];
     const idAudio = "#som_"+efeito;
-    listaDeTeclas(contador).onclick = function(){
-        tocasom("#som_tecla_aplausos");
-        tocasom(idAudio);
-    
+    listaDeTeclas[contador].onclick = function(){
+        tocasom (idAudio)     
     }
     contador = contador +1;
     console.log(contador);
