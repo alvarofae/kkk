5a4114edf71027e4b33d4b4cd4654b9f7e0717b7
alvarofae <105504676+alvarofae@users.noreply.github.com>
2023-11-09 08:21:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert("Trator")
+function tocasom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}   
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while(contador<listaDeTeclas.lenght){
+    const efeito = listaDeTeclas(contador).classlist[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas(contador).onclick = function(){
+        tocasom("#som_tecla_aplausos");
+        tocasom(idAudio);
+    
+    }
+    contador = contador +1;
+    console.log(contador);
+    }
+
